5d6f2c003bd2d9fb581d45b1e07d5b8c01545a7f
Aryan Mishra <mishraaryan2107@gmail.com>
2026-07-20 14:52:04 +0530

    add changes in mainjsx file

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store.js";
-import TanstackProvider from "./layouts/provider/tanstackProvider.jsx";
+import TanstackProvider from "./layouts/provider/TanstackProvider";
 import { Toaster } from "sonner";
 createRoot(document.getElementById("root")).render(
   <TanstackProvider>
